337e8c4eb780f8ce812dc7c220f1fcb26430d7c8
凡猫 <leiqifan.lqf@alibaba-inc.com>
2025-02-07 17:47:03 +0800

    feat: init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./global.css";
+import Sidebar from "@/components/Sidebar";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">
+          <div className="main">
+            <Sidebar />
+            <section className="col note-viewer">{children}</section>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
